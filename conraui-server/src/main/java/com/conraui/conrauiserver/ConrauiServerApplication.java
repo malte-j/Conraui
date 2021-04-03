e3b7d405a662db10cce0bcd712cf36ebe168ca36
Malte <skyguardian42@gmail.com>
2021-04-03 14:38:56 +0200

    initial commit

---
+++ conraui-server/src/main/java/com/conraui/conrauiserver/ConrauiServerApplication.java
@@ -0,0 +1,13 @@
+package com.conraui.conrauiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConrauiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConrauiServerApplication.class, args);
+	}
+
+}
